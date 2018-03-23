99db324b8929c318d37de2459afd9a2aea9d909e
neilsatt <neilsatt@gmail.com>
2018-03-22 19:50:18 -0600

    add products and ratings components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import {Button} from 'react-bootstrap';
+import Products from './Products';
 
 
 class App extends Component {
   render() {
+    const isValid = true;
+
     return (
       <div>
           <h1>First App</h1>
+          <Products />
+          <Button bsStyle="primary" disabled={!isValid}>default</Button>
       </div>
     );
   }
