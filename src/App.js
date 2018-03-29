b2affe0e1575fde2544b10ef1b37569c23bcaf1c
neilsatt <neilsatt@gmail.com>
2018-03-28 20:20:44 -0600

    chapter 4

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React, { Component } from 'react';
-import {Button} from 'react-bootstrap';
 import Products from './Products';
-import Rating from './Rating';
-
 
 class App extends Component {
-  render() {
-    const isValid = true;
-
+  render() {        
     return (
       <div>
-        <Rating rating="1" />
-        <Rating rating="2" />
-        <Rating rating="3" />
-        <Rating rating="4" />
-        <Rating rating="5" />
+        <Products />        
       </div>
     );
   }
